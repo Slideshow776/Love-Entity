94e7188aa9e05c35555dc941327f12e378a20583
Sandra Moen <sandramoen01@gmail.com>
2020-01-04 11:43:18 +0100

    mobile environment setup

--- android/src/no/sandramoen/loveentity/AndroidLauncher.java
+++ android/src/no/sandramoen/loveentity/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.sandramoen.loveentity.LoveEntityGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
